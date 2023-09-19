8d7dfe56fba0df80733909f79eb66eaa77566993
harkgill123 <harkirat.s.gill@ryerson.ca>
2023-09-19 14:00:09 -0400

    Connected DB

--- jobML/settings.py
+++ jobML/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'jobML.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Mysql',
+        'USER': 'harkgill',
+        'PASSWORD': 'harkgill_TMU',
+        'HOST': 'localhost',  
+        'PORT': '',           
     }
 }
 
