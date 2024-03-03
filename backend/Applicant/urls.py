8cb990f9ee86e519325e7a2253b4615505bf804d
Harkirat <Harkirat.s.gill@ryerson.ca>
2024-03-02 19:01:34 -0500

    urls

--- backend/Applicant/urls.py
+++ backend/Applicant/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ResumeCreateView, ResumeUploadView, search_jobs
+from .views import ResumeCreateView, ResumeUploadView, search_jobs, recommended_jobs
 
 
 urlpatterns = [
     path('upload-resume/', ResumeUploadView.as_view(), name='upload-resume'),
     path('create-resume/', ResumeCreateView.as_view(), name='create-resume'),
-    path('search-jobs/', search_jobs, name='search-jobs')
+    path('search-jobs/', search_jobs, name='search-jobs'),
+    path('recommend-jobs/', recommended_jobs, name='recommend-jobs')
 ]
