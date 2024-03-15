5099ffb99211664c3dfbb2a7d55eb65329180b74
Harkirat <Harkirat.s.gill@ryerson.ca>
2024-03-14 22:50:24 -0400

    UserDisplayView

--- backend/Recruiter/serializers.py
+++ backend/Recruiter/serializers.py
@@ -3,6 +3,8 @@ from UserAuth.models import JobPosting,  ListOfSkills
 from rest_framework import serializers
 from UserAuth.models import JobPosting
 from django.db import transaction
+from rest_framework import serializers
+from UserAuth.models import User, Resume, Education, WorkExperience, ResumeToSkills
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
@@ -61,3 +63,34 @@ class JobPostingSerializer(serializers.ModelSerializer):
             'posted_date', 'application_deadline', 'requirements', 
             'job_skills', 'benefits', 'employment_type'
         )
+
+class ResumeToSkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResumeToSkills
+        fields = ['skill_name']
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = ['school_name', 'degree', 'start_date', 'end_date', 'gpa']
+
+class WorkExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkExperience
+        fields = ['company_name', 'job_title', 'start_date', 'end_date', 'job_description']
+
+class ResumeSerializer(serializers.ModelSerializer):
+    resume_skills = ResumeToSkillsSerializer(many=True, read_only=True)
+    educations = EducationSerializer(many=True, read_only=True)
+    work_experiences = WorkExperienceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Resume
+        fields = ['resume_skills', 'educations', 'work_experiences']
+
+class UserSerializer(serializers.ModelSerializer):
+    resumes = ResumeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'name', 'email', 'phone_number', 'user_type', 'resumes']
