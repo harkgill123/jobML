d489666ecbfe9ca9a764421a9f5c46dc08a3a269
ksoubra1 <ksoubra1@gmail.com>
2024-03-16 13:20:09 -0400

    resolving merging conflict

--- backend/UserAuth/urls.py
+++ backend/UserAuth/urls.py
@@ -1,15 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import SignUpView, LoginView, LogoutView, UpdatePasswordView, UpdateEmailView, DisplayAllJobsInfo, DisplayResumeInfo, UpdateResumeInfo, UpdateJobPosting
+from .views import SignUpView, LoginView, LogoutView, UpdatePasswordView, UpdateEmailView,DisplayResumeInfo
 urlpatterns = [
     path('Signup/', SignUpView.as_view(), name='Signup'),
     path('Login/', LoginView.as_view(), name='Login'),
     path('Logout/', LogoutView.as_view(), name = 'Logout'),
     path('update-email/', UpdateEmailView.as_view(), name = 'update-email'),
     path('update-password/', UpdatePasswordView.as_view(), name = 'update-password'),
-    path('display-resume/', DisplayResumeInfo.as_view(), name = 'display-resume'),
-    path('display-jobs-info/', DisplayAllJobsInfo.as_view(), name = 'display-jobs-info'),
-    path('edit-resume/', UpdateResumeInfo.as_view(), name = 'edit-resume'),
-    path('edit-job-postings/', UpdateJobPosting.as_view(), name = 'edit-job-postings')
+    path('display-resume/',DisplayResumeInfo.as_view(),name='disaply-resume')
 ]
